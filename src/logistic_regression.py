782431b2d1c805c083a447aefdb9f2b7391591d7
Duc-Anh Luu <Duc-Anh.Luu@grenoble-inp.org>
2017-09-03 23:11:13 +0200

    Add core functions for logistic regresstion Very first basic model: High bias problem

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -10,6 +10,6 @@ train_set_x_orig, train_set_y, test_set_x_orig = load_dataset(train_set, test_se
 train_set_x_orig, train_set_y, order = shuffle_data(train_set_x_orig, train_set_y)
 
 #- Devide dataset into train/dev/test set
+X_train, Y_train, X_dev, Y_dev, X_test, Y_test = divide_dataset(20, train_set_x_orig, train_set_y)
 
-x_train, y_train, x_dev, y_dev, x_test, y_test = divide_dataset(20, train_set_x_orig, train_set_y)
-print(x_train[:, 0])
+model(X_train, Y_train, X_dev, Y_dev, X_test, Y_test, num_epochs=20000, learning_rate = 0.05, print_cost = True)
