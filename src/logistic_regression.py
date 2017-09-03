1aad9eb9b8465fd5036d3435ceff2eaadb17634b
ducanhluu <ducanhluu2310@gmail.com>
2017-09-03 20:35:52 +0200

    Add divide_dataset function which allows cross-validation

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -8,4 +8,8 @@ train_set_x_orig, train_set_y, test_set_x_orig = load_dataset(train_set, test_se
 
 #- Shuffle data
 train_set_x_orig, train_set_y, order = shuffle_data(train_set_x_orig, train_set_y)
-print(train_set_x_orig[:, 0])
+
+#- Devide dataset into train/dev/test set
+
+x_train, y_train, x_dev, y_dev, x_test, y_test = divide_dataset(20, train_set_x_orig, train_set_y)
+print(x_train[:, 0])
