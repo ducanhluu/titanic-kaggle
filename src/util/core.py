7a35b787f6d28d9ae57ed138b9c383d56604b15b
ducanhluu <ducanhluu2310@gmail.com>
2017-09-10 22:59:20 +0200

    Fix bug of deep learning implementation: wrong backward implementation of sigmoid

--- src/util/core.py
+++ src/util/core.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+np.random.seed(1)
+np.seterr(divide='ignore', invalid='ignore')
+
 def initialize_parameters(n_x, n_y):
 	"""
     Argument:
@@ -12,8 +15,7 @@ def initialize_parameters(n_x, n_y):
                     W1 -- weight matrix of shape (n_y, n_x)
                     b1 -- bias vector of shape (n_y, 1)
     """
-
-	W1 = np.zeros((n_y, n_x))
+	W1 = np.random.randn(n_y, n_x) * 0.01
 	b1 = np.zeros((n_y, 1))
 
 	parameters = {
@@ -27,23 +29,27 @@ def sigmoid(z):
 	return 1 / (1 + np.exp(-z))
 
 def sigmoid_forward(z):
+	g = 1 / (1 + np.exp(-z))
 	cache = {
-		"Z" : z
-	} 
-	return 1 / (1 + np.exp(-z)), cache
+		"G" : g
+	}
+	
+	return g, cache
 
 def relu_forward(z):
 	cache = {
 		"Z" : z
 	}
+
 	if z > 0:
 		return z, cache
 	return 0, cache
 
 def sigmoid_backward(dA, cache):
-	Z = cache["Z"]
-	dAdZ = Z * (1 - Z)
-	dZ = np.multiply(dA, dAdZ)	
+	G = cache["G"]
+	dAdZ = G * (1 - G)
+	dZ = np.multiply(dA, dAdZ)
+
 	return dZ
 
 def relu_backward(dA, cache):
@@ -54,7 +60,9 @@ def relu_backward(dA, cache):
 
 def compute_cost(Y_pred, Y):
 	m = Y.shape[1]
-	return (-1 / m) * np.sum(np.multiply(Y, np.log(Y_pred)) + np.multiply((1 - Y), np.log(1 - Y_pred)), axis=1)
+	cost = (-1 / m) * np.sum(np.multiply(Y, np.log(Y_pred)) + np.multiply((1 - Y), np.log(1 - Y_pred)), axis=1)
+	cost = np.squeeze(cost)
+	return cost
 
 def propagate(W, b, X, Y):
 	m = X.shape[1]
@@ -80,13 +88,11 @@ def optimize(W, b, X, Y, num_epochs, learning_rate, print_cost = False):
 		db = grads["db"]
 
 		W = W - learning_rate * dW
-		b = b - learning_rate * db
-
-		if i % 100 == 0:
-			costs.append(cost)
+		b = b - learning_rate * db			
 
 		if i % 100 == 0 and print_cost:
 			print("Cost after iteration %i: %f" %(i, cost))
+			costs.append(cost)
 
 	params = {
 		"W" : W,
@@ -101,7 +107,7 @@ def logistic_regression_predict(X, parameters):
 	Y_pred = sigmoid(np.dot(W, X) + b) > 0.5
 	return Y_pred
 
-def model(X_train, Y_train, X_dev, Y_dev, X_test, Y_test, num_epochs=2000, learning_rate = 0.5, print_cost = False):
+def model(X_train, Y_train, num_epochs=2000, learning_rate = 0.5, print_cost = False):
 	parameters = initialize_parameters(X_train.shape[0], Y_train.shape[0])
 	W = parameters["W"]
 	b = parameters["b"]
@@ -153,7 +159,6 @@ def L_model_forward(X, parameters):
 	caches = []
 	#- Number of layers in neural network
 	L = len(parameters) // 2
-
 	for l in range(1, L):
 		A_prev = A
 		A, cache = linear_activation_forward(A_prev, parameters["W" + str(l)], parameters["b" + str(l)], activation = "relu")
