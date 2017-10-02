e3f5094bc858084b1da2231b14441d6024169752
ducanhluu <ducanhluu2310@gmail.com>
2017-10-02 20:37:59 +0200

    Dealing with missing value in Embarked variable

--- src/analyse_data.R
+++ src/analyse_data.R
@@ -1,7 +1,10 @@
 Sys.setenv(LANG = "en")
 #- Load some useful packages
-#library('ggplot2')
+library('ggplot2')
+library('ggthemes')
 library('dplyr')
+library('mice') # imputation
+library('scales')
 
 #- Load data
 train_data <- read.csv("./data/train.csv")
@@ -31,3 +34,29 @@ table(full$Sex, full$Title)
 
 #- Grab surname from passenger name
 full$Surname <- sapply(full$Name, function(x) strsplit(x, split = '[,.]')[[1]][1])
+#cat(paste('We have <b>', nlevels(factor(full$Surname)), '</b> unique sur names'))
+
+#- Create a family size variable including the passenger themselves
+full$Fsize <- full$SibSp + full$Parch + 1
+
+full$Family <- paste(full$Surname, full$Fsize, sep = "_")
+
+ggplot(full[1:891,], aes(x = Fsize, fill = factor(Survived))) +
+  geom_bar(stat='count', position='dodge') +
+  scale_x_continuous(breaks=c(1:11)) +
+  labs(x = 'Family Size') +
+  theme_few()
+
+#- Create discret family size variable
+full$FsizeD[full$Fsize == 1] <- "singleton"
+full$FsizeD[full$Fsize > 1 & full$Fsize < 5] <- "small"
+full$FsizeD[full$Fsize > 4] <- "large"
+
+#- Showing a mosaic plot
+mosaicplot(table(full$FsizeD, full$Survived), main = "Family Size by Survival", shade = TRUE)
+
+#- Create a Deck variable
+full$Deck <- sapply(full$Cabin, function(x) strsplit(x, NULL)[[1]][1])
+
+embark_fare <- full %>% filter(PassengerId != 62 & PassengerId != 830)
+print(embark_fare)
